38d91ae5b6372bc51b425c0a817288fe68343fa9
Aladdin <aladdin19.11.21@gmail.com>
2025-09-08 16:02:47 +0400

    fix

--- src/main/java/com/team/updevic001/utility/screenshot/PlaywrightConfig.java
+++ src/main/java/com/team/updevic001/utility/screenshot/PlaywrightConfig.java
@@ -1,31 +1,31 @@
-package com.team.updevic001.utility.screenshot;
-
-import com.microsoft.playwright.*;
-import jakarta.annotation.PreDestroy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-
-@Configuration
-public class PlaywrightConfig {
-
-    private Playwright playwright;
-    private Browser browser;
-
-    @Bean
-    public Browser chromiumBrowser() {
-        playwright = Playwright.create();
-        BrowserType.LaunchOptions launch = new BrowserType.LaunchOptions()
-                .setHeadless(true)
-                .setArgs(Arrays.asList(new String[]{"--no-sandbox", "--disable-setuid-sandbox"}));
-        browser = playwright.chromium().launch(launch);
-        return browser;
-    }
-
-    @PreDestroy
-    public void shutdown() {
-        if (browser != null) browser.close();
-        if (playwright != null) playwright.close();
-    }
-}
+//package com.team.updevic001.utility.screenshot;
+//
+//import com.microsoft.playwright.*;
+//import jakarta.annotation.PreDestroy;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//public class PlaywrightConfig {
+//
+//    private Playwright playwright;
+//    private Browser browser;
+//
+//    @Bean
+//    public Browser chromiumBrowser() {
+//        playwright = Playwright.create();
+//        BrowserType.LaunchOptions launch = new BrowserType.LaunchOptions()
+//                .setHeadless(true)
+//                .setArgs(Arrays.asList(new String[]{"--no-sandbox", "--disable-setuid-sandbox"}));
+//        browser = playwright.chromium().launch(launch);
+//        return browser;
+//    }
+//
+//    @PreDestroy
+//    public void shutdown() {
+//        if (browser != null) browser.close();
+//        if (playwright != null) playwright.close();
+//    }
+//}
